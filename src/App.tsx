c90cb681b747ef13390f8a5d5de40fda7b8f7715
Nathan Dunn <nathand@truveta.com>
2021-11-08 15:47:13 -0800

    got jbrowse workingn beautifully

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,114 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {createViewState, JBrowseLinearGenomeView} from "@jbrowse/react-linear-genome-view";
+
+const assembly = {
+    name: 'GRCh38',
+    sequence: {
+        type: 'ReferenceSequenceTrack',
+        trackId: 'GRCh38-ReferenceSequenceTrack',
+        adapter: {
+            type: 'BgzipFastaAdapter',
+            fastaLocation: {
+                uri: 'https://s3.amazonaws.com/jbrowse.org/genomes/GRCh38/fasta/GRCh38.fa.gz',
+                locationType: 'UriLocation',
+            },
+            faiLocation: {
+                uri: 'https://s3.amazonaws.com/jbrowse.org/genomes/GRCh38/fasta/GRCh38.fa.gz.fai',
+                locationType: 'UriLocation',
+            },
+            gziLocation: {
+                uri: 'https://s3.amazonaws.com/jbrowse.org/genomes/GRCh38/fasta/GRCh38.fa.gz.gzi',
+                locationType: 'UriLocation',
+            },
+        },
+    },
+    aliases: ['hg38'],
+    refNameAliases: {
+        adapter: {
+            type: 'RefNameAliasAdapter',
+            location: {
+                uri: 'https://s3.amazonaws.com/jbrowse.org/genomes/GRCh38/hg38_aliases.txt',
+                locationType: 'UriLocation',
+            },
+        },
+    },
+}
+
+const tracks = [
+    {
+        type: 'FeatureTrack',
+        trackId: 'ncbi_refseq_109_hg38',
+        name: 'NCBI RefSeq (GFF3Tabix)',
+        assemblyNames: ['GRCh38'],
+        category: ['Annotation'],
+        adapter: {
+            type: 'Gff3TabixAdapter',
+            gffGzLocation: {
+                uri: 'https://s3.amazonaws.com/jbrowse.org/genomes/GRCh38/ncbi_refseq/GCA_000001405.15_GRCh38_full_analysis_set.refseq_annotation.sorted.gff.gz',
+                locationType: 'UriLocation',
+            },
+            index: {
+                location: {
+                    uri: 'https://s3.amazonaws.com/jbrowse.org/genomes/GRCh38/ncbi_refseq/GCA_000001405.15_GRCh38_full_analysis_set.refseq_annotation.sorted.gff.gz.tbi',
+                    locationType: 'UriLocation',
+                },
+            },
+        },
+    },
+]
+
+const defaultSession = {
+    name: 'My session',
+    view: {
+        id: 'linearGenomeView',
+        type: 'LinearGenomeView',
+        tracks: [
+            {
+                type: 'ReferenceSequenceTrack',
+                configuration: 'GRCh38-ReferenceSequenceTrack',
+                displays: [
+                    {
+                        type: 'LinearReferenceSequenceDisplay',
+                        configuration:
+                            'GRCh38-ReferenceSequenceTrack-LinearReferenceSequenceDisplay',
+                    },
+                ],
+            },
+            {
+                type: 'FeatureTrack',
+                configuration: 'ncbi_refseq_109_hg38',
+                displays: [
+                    {
+                        type: 'LinearBasicDisplay',
+                        configuration: 'ncbi_refseq_109_hg38-LinearBasicDisplay',
+                    },
+                ],
+            },
+        ],
+    },
+}
 
 function App() {
+
+    const state = createViewState({
+        assembly,
+        tracks,
+        location: '10:29,838,737..29,838,819',
+        defaultSession,
+    })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+          JBrowse Genome View
       </header>
+        <div>
+
+            <JBrowseLinearGenomeView viewState={state} />
+        </div>
+
+
     </div>
   );
 }
